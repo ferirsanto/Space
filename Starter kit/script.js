ca5b6abe850493baa4f9784c9134c3192b6122b0
FeriPujianto <pujianto@uni-bremen.de>
2017-10-03 14:32:28 +0200

    Starter kit pushed

---
+++ Starter kit/script.js
@@ -0,0 +1,12 @@
+console.log('Script running');
+//Wait till all content is loaded, could be external fonts scripts from other servers etc....
+if (document.readyState != 'loading'){
+  onDocumentReady();
+} else {
+  document.addEventListener('DOMContentLoaded', onDocumentReady);
+}
+
+// Page is loaded! Now event can be wired-up
+function onDocumentReady() {
+  console.log('Document ready.');
+}
