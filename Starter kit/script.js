4670f5bfb0f590112ad58848decae4c0b2999f3f
FeriPujianto <pujianto@uni-bremen.de>
2017-10-05 16:07:41 +0200

    Sorted by colour

--- Starter kit/script.js
+++ Starter kit/script.js
@@ -12,6 +12,8 @@ function onDocumentReady() {
 }
 
 var divList = document.getElementsByTagName('div');
+var array = [].slice.call(divList, 0);
+console.log(array[0]);
 
 document.body.onkeyup = function(e) {
   if (e.keyCode == 32) {
@@ -20,5 +22,12 @@ document.body.onkeyup = function(e) {
 }
 
 function showHiddenDivs() {
+  // document.getElementById('text1').style.visibility = 'visible';       Tester
+  var rand = array[Math.floor(Math.random() * array.length)];
+  var index = array.indexOf(rand);
+
+    array.splice(index, 1);
+    rand.style.visibility = 'visible';
+    // console.log(rand);
 
 }
